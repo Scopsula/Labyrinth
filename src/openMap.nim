200ac0bb86df83f0846ee2e16b5eded73950dc84
Scopsula <scopsula@outlook.com>
2025-09-09 22:16:02 +0100

    Map System + Fixes

---
+++ src/openMap.nim
@@ -0,0 +1,74 @@
+import os, strutils
+
+proc openMap*(wh: array[2, int], loc: array[4, int], m: string, mW: int, h0: int) =
+  var paths: string = readFile("../loadedLevel/map")
+  let mYC = paths.splitLines.len
+  paths = paths.replace("\n", "")
+  for i in 0 .. m.len - 1:
+    if m[i] == '9': 
+      paths[i] = 'C'
+    if m[i] == 'S':
+      paths[i] = 'S'
+    if h0 == 1:
+      if m[i] == 'X':
+        paths[i] = 'X'
+
+  let
+    yD = (wh[1] - 1) div 2
+    xD = (wh[0] - 1) div 2
+    pos = loc[1] * mW + loc[0]
+
+  var 
+    lXB: int
+    uXB: int
+    lYB: int
+    uYB: int
+
+  if loc[0] >= xD: lXB = pos - xD 
+  else: lXB = pos - loc[0]
+  if loc[0] + xD < mW: uXB = pos + xD
+  else: uXB = pos - loc[0] + mW - 1
+  if loc[1] >= yD: lYB = -yD
+  else: lYB = -(yD + (loc[1] - yD))
+  if loc[1] + yD <= mYC - 2: uYB = yD 
+  else: uYB = yD - (loc[1] + yD - mYC + 2)
+
+  var visible: string
+  for i in lYB .. uYB:
+    let lY: int = i * mW
+    let l: string = paths[lXB + lY .. uXB + lY]
+    if i < uYB: visible = visible & l & "\n"
+    else: visible = visible & l
+
+  let x: int = visible.splitLines[0].len
+  let y: int = visible.splitLines.len
+
+  for b in 0 .. x - 1:
+    visible[b] = '-'
+    visible[(y - 1) * (x + 1) + b] = '-'
+  for b in 0 .. y - 1:
+    visible[b * (x + 1)] = '|'
+    visible[b * (x + 1) + x - 1] = '|'
+
+  let 
+    l1: string = " Some areas may be unmarked |"
+    l2: string = " S: --Player--------------- |"
+    l3: string = " C: --Tiles Visited-------- |"
+    l4: string = " :: --[m] to close map----- |"
+    lD: string = "----------------------------|"
+
+  if h0 == 1:
+    let l5: string = " X: --Exit----------------- |"
+    visible[5 * (x + 1) + 1  .. 5 * (x + 1) + l5.len] = l5
+    visible[6 * (x + 1) + 1  .. 6 * (x + 1) + lD.len] = lD
+  else: 
+    visible[5 * (x + 1) + 1  .. 5 * (x + 1) + lD.len] = lD
+
+  visible[x + 2 .. x + 1 + l1.len] = l1
+  visible[2 * (x + 1) + 1  .. 2 * (x + 1) + l2.len] = l2
+  visible[3 * (x + 1) + 1  .. 3 * (x + 1) + l3.len] = l3
+  visible[4 * (x + 1) + 1  .. 4 * (x + 1) + l4.len] = l4
+
+  discard execShellCmd("clear")
+  echo visible
+
--- src/player.nim
+++ src/player.nim
@@ -1,5 +1,5 @@
 import terminal, strutils, random
-import screen
+import screen, style, openMap
 
 const
   tX: int = 10
@@ -19,9 +19,8 @@ let
   xD = (scX - 1) div 2 + 2
 
 var h0: int = 0
-echo "Enable Coordinates? [y/n]"
-let eC: string = readLine(stdin).toUpper
-if eC == "Y": h0 = 1
+let conf = readFile("../config").splitLines
+if conf[1].split(' ')[1] == "true": h0 = 1
 
 randomize()
 newLevel()
@@ -63,19 +62,18 @@ proc update() =
   visible = ""
   let pos = y * mW + x
 
-  var lXB: int
+  var 
+    lXB: int
+    uXB: int
+    lYB: int
+    uYB: int
+
   if x >= xD: lXB = pos - xD 
   else: lXB = pos - x
-
-  var uXB: int
   if x + xD < mW: uXB = pos + xD
   else: uXB = pos - x + mW - 1
-
-  var lYB: int
   if y >= yD: lYB = -yD
   else: lYB = -(yD + (y - yD))
-
-  var uYB: int
   if y + yD <= mYC - 2: uYB = yD 
   else: uYB = yD - (y + yD - mYC + 2)
 
@@ -94,40 +92,57 @@ proc getKey(): string =
   else:
     return $fC
 
-var up: bool = true
+var 
+  up: bool = true
+  bypass: bool = true
+  closeMap: bool = false
+
 while true:
   if up == true:
     m[y * mW + x] = 'S'
     update()
     m = sc(visible, [w, h, tX, tY], [x, y], [gX, gY, h0], [xD, yD, mW, mYC], m)
     up = false
-  let input = getKey()
-  case input
-  of "a", "h", "left":
-    if x - 1 >= 0:
-      if m[y * mW + x - 1] != ' ':
-        m[y * mW + x] = '*'
-        x -= 1
-        up = true
-  of "d", "l", "right":
-    if x + 1 <= mW - 1:
-      if m[y * mW + x + 1] != ' ':
-        m[y * mW + x] = '*'
-        x += 1
-        up = true
-  of "w", "k", "up":
-    if (y - 1) * mW + x >= 0:
-      if m[(y - 1) * mW + x] != ' ':
-        m[y * mW + x] = '*'
-        y -= 1
-        up = true
-  of "s", "j", "down":
-    if (y + 1) * mW + x < m.len:
-      if m[(y + 1) * mW + x] != ' ':
-        m[y * mW + x] = '*'
-        y += 1
-        up = true
-  of "q":
-    showCursor()
-    quit(0)
-  else: discard
+  if bypass == true:
+    bypass = false
+    if refresh() == true or closeMap == true:
+      closeMap = false
+      up = true
+  else:
+    let input = getKey()
+    case input
+    of "a", "h", "left":
+      if x - 1 >= 0:
+        if m[y * mW + x - 1] != ' ':
+          m[y * mW + x] = '9'
+          x -= 1
+          up = true
+    of "d", "l", "right":
+      if x + 1 <= mW - 1:
+        if m[y * mW + x + 1] != ' ':
+          m[y * mW + x] = '9'
+          x += 1
+          up = true
+    of "w", "k", "up":
+      if (y - 1) * mW + x >= 0:
+        if m[(y - 1) * mW + x] != ' ':
+          m[y * mW + x] = '9'
+          y -= 1
+          up = true
+    of "s", "j", "down":
+      if (y + 1) * mW + x < m.len:
+        if m[(y + 1) * mW + x] != ' ':
+          m[y * mW + x] = '9'
+          y += 1
+          up = true
+    of "m":
+      openMap([w, h], [x, y, gX, gY], m, mW, h0)
+      while true:
+        if getch() == 'm':
+          break
+      bypass = true
+      closeMap = true
+    of "q":
+      showCursor()
+      quit(0)
+    else: discard
