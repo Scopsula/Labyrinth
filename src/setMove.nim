3c0c1cf68bdd6642908b1f5889759b2154f0f43e
Scopsula <scopsula@outlook.com>
2025-10-14 10:38:14 +0100

    Fixes maybe

--- src/setMove.nim
+++ src/setMove.nim
@@ -2,11 +2,11 @@ proc selEMove*(eType: string, turn: int): string =
   let entity = eType[9 .. ^2]
   case entity
   of "smiler":
-    if turn == 1:
-      return "chase"
-    if turn mod 3 == 0:
-      return "darkEnergyExpansion"
     if turn mod 2 == 0:
       return "bite"
+    elif turn < 5:
+      return "chase"
+    elif turn > 4:
+      return "darkEnergyExpansion"
   else:
     discard
