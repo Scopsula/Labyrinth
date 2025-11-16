37c65bf949587edde75ef45faf3283e9521aa63e
Scopsula <scopsula@tuta.com>
2025-11-16 00:01:00 +0000

    Adjust Zone

--- src/style.nim
+++ src/style.nim
@@ -12,7 +12,7 @@ let loot: string = readFile("../data/config").splitLines[5].split(' ')[1]
 proc audioZone*(xy: array[2, int], t: array[2, int], lv: int): string =
   case lv
   of 1:
-    let nx: string = &"{xy[0] div (t[0] * t[1] + 1)}"
+    let nx: string = &"{(xy[0] + 1) div (t[0] * t[1] + 1)}"
     let ny: string = &"{xy[1] div (t[1] * t[1])}"
     if "13579".contains(nx[^1]) and "13579".contains(ny[^1]):
       return "halls"
