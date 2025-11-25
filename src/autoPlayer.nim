032c1989fee8d11bf43fc794b5fb0bc2281c85f6
Scopsula <scopsula@outlook.com>
2025-11-25 11:34:22 +0000

    Stuff

--- src/autoPlayer.nim
+++ src/autoPlayer.nim
@@ -67,7 +67,7 @@ proc main() =
   let sMap: string = map
   let mYC: int = map.splitLines.len
   let mW: int = map.splitlines[0].len
-  setRValues(lv, map, [mYC, mW, tx, ty])
+  setRValues(lv, [mYC, mW, tx, ty])
 
   var gX: int
   var gY: int
