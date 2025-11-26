57a127608412127450673ba7624638e25e5a1983
Scopsula <scopsula@outlook.com>
2025-11-26 09:04:13 +0000

    ....

--- src/style.nim
+++ src/style.nim
@@ -170,7 +170,7 @@ proc adjustVisible*(v: string, xy: array[2, int], level: int, mS: array[2, strin
         var nx: int = (xy[0] - coords[0] + x) div (t[0] * t[1] + 1)
         var ny: int = (xy[1] - coords[1] + y) div (t[1] * t[1])
         if rValues[nx + (ny * rValues[0]) + 1] == 1:
-          if doRValues == true and nC == true:
+          if nC == true:
             noCorner(nx, ny)
           let cx: int = xy[0] - coords[0] + x - (nx * (t[0] * t[1] + 1))
           let cy: int = xy[1] - coords[1] + y - (ny * (t[1] * t[1]))
