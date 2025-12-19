50545dcf55394357101c50a350e34e639f8ee3cb
Scopsula <scopsula@outlook.com>
2025-12-19 10:52:55 +0000

    Level 3 Under Construction

--- src/custGen.nim
+++ src/custGen.nim
@@ -25,6 +25,23 @@ proc cGen*(n: int, t: array[2, int]): seq[array[2, int]] =
         pos[d[0]] += d[1]
         path.add(pos)
     return path
+  of "3":
+    for i in 1 .. n:
+      let selW: int = rand(1 .. 100) div 30
+      let d: array[2, int] = sample([[0, 1], [0,-1], [1,1], [1,-1]])
+      for i in 1 .. rand(t[1] .. (t[0] - d[0] * t[1])):
+        proc doW() =
+          var oD: int = d[0] - 1
+          if oD == -1: oD = 0
+          var tPos = pos
+          tPos[oD] -= 1 + (selW div 2)
+          while tPos[oD] < pos[oD] + selW div 2:
+            tPos[oD] += 1
+            path.add(tPos)
+        if i == 1: doW()
+        pos[d[0]] += d[1]
+        doW()
+    return path
   of "4":
     for i in 1 .. n * rand(1 .. t[0]):
       let d: array[2, int] = sample([[0, 1], [0,-1], [1,1], [1,-1]])
